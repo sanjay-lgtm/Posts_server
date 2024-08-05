425cad23b12c95a40062d0c102e9bf8d0668719b
sanjay <113435192+sanjay-lgtm@users.noreply.github.com>
2024-08-06 03:50:54 +0530

    post

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
-
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import postRoutes  from './routes/postRoutes.js';
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -10,7 +12,10 @@ mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('MongoDB connected 🌐'))
 .catch(err => console.log(err));
 
+app.use(cors());
+app.use(bodyParser.json());
 
+app.use('/api',postRoutes );
 
 
 
---
+++ models/Post.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+
+const PostSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+export const Post = mongoose.model('Post', PostSchema);
