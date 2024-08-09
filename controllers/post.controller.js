a7f9aba05cd04dabfbfbe99b1c8962ca627d61e3
sanjay <113435192+sanjay-lgtm@users.noreply.github.com>
2024-08-09 22:34:32 +0530

    Post controller done.

---
+++ controllers/post.controller.js
@@ -0,0 +1,287 @@
+import sharp from "sharp";
+import cloudinary from "../utils/cloudinary.js";
+import { Post } from "../models/post.models.js";
+import { User } from "../models/user.models.js";
+
+export const addNewPost = async (req, res) => {
+    try {
+        const { caption } = req.body;
+        const image = req.file;
+        const authorId = req.id;
+        if (!image) {
+            return res.status(400).json({
+                message: "Image required!"
+            })
+        }
+
+        const optimizedImageBuffer = await sharp(image.buffer)
+            .resize({ width: 800, height: 800, fit: 'inside' })
+            .toFormat('jpeg', { quality: 80 })
+            .toBuffer();
+
+        // buffer to data uri
+
+        const fileUri = `data:image/jpeg;base64,${optimizedImageBuffer.toString('base64')}`;
+
+        const cloudResponse = await cloudinary.uploader.upload(fileUri);
+        const post = await Post.create({
+            caption,
+            image: cloudResponse.secure_url,
+            author: authorId
+        });
+
+        const user = await User.findById(authorId);
+        if (user) {
+            user.posts.push(post._id);
+            await user.save();
+        }
+
+        await post.populate({ path: 'author', select: '-password' });
+
+        return res.status(201).json({
+            message: "New post added",
+            post,
+            success: true
+        })
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+export const getAllPost = async (req, res) => {
+    try {
+        const posts = await Post.find().sort({ createdAt: -1 })
+            .populate({ path: 'author', select: 'username , profilePicture' })
+            .populate({
+                path: 'comments',
+                sort: { createdAt: -1 },
+                populate: {
+                    path: 'author',
+                    select: 'username , profilePicture'
+                }
+            })
+        return res.status(200).json({
+            posts,
+            success: true
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const getUserPost = async (req, res) => {
+    try {
+        const authorId = req.id;
+        const posts = await Post.find({ author: authorId }).sort({ createdAt: -1 }).populate({
+            path: 'author',
+            select: 'username , profilePicture'
+        }).populate({
+            path: 'comments',
+            sort: { createdAt: -1 },
+            populate: {
+                path: 'author',
+                select: 'username , profilePicture'
+            }
+        })
+        return res.status(200).json({
+            posts,
+            success: true
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const likePost = async (req, res) => {
+    try {
+        const likeKarneWalaUserKiId = req.id;
+        const postId = req.params.id;
+        const post = await Post.findById(postId);
+        if (!post) {
+            return res.status(404).json({
+                message: 'Post not found',
+                success: false
+            })
+        }
+        //like logic
+        await post.updateOne({ $addToSet: { likes: likeKarneWalaUserKiId } })
+        await post.save();
+
+        //impliment socketio for real time notification
+
+        return res.status(201).json({
+            message: 'Post liked',
+            success: true
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+export const dislikePost = async (req, res) => {
+    try {
+        const likeKarneWalaUserKiId = req.id;
+        const postId = req.params.id;
+        const post = await Post.findById(postId);
+        if (!post) {
+            return res.status(404).json({
+                message: 'Post not found',
+                success: false
+            })
+        }
+        //like logic
+        await post.updateOne({ $pull: { likes: likeKarneWalaUserKiId } })
+        await post.save();
+
+        //impliment socketio for real time notification
+
+        return res.status(201).json({
+            message: 'Post disliked',
+            success: true
+        })
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const addComment = async (req, res) => {
+    try {
+        const postId = req.params.id;
+        const commentKarneWalaKiId = req.id;
+
+        const { text } = req.body;
+        const post = await Post.findById(postId);
+        if (!text) {
+            return res.status(400).json({
+                message: 'Comment text is required',
+                success: false
+            })
+        }
+
+        const comment = await Comment.create({
+            text,
+            author: commentKarneWalaKiId,
+            postId: postId
+        })
+        await comment.populate({
+            path: 'author',
+            select: "username, profilePicture"
+        });
+
+        post.comments.push(comment._id);
+        await post.save();
+
+        return res.status(201).json({
+            message: 'Comment Added',
+            comment,
+            success: true
+        })
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const getCommentsOfPost = async (req, res) => {
+    try {
+        const postId = req.params.id;
+
+        const comments = await Comment.find({ post: postId }).populate('author', 'username', 'profilePicture');
+        if (!comments) {
+            return res.status(404).json({
+                message: 'No comments found',
+                success: false
+            })
+        }
+        return res.status(201).json({
+            comments,
+            success: true
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+export const deletePost = async (req, res) => {
+    try {
+        const postId = req.params.id;
+        const authorId = req.id;
+        const post = await Post.findById(postId);
+        if (!post) {
+            return res.status(404).json({
+                message: 'Post not found',
+                success: false
+            })
+        }
+        //check if the logged in user is the owner of the post
+        if (post.author.toString() !== authorId) {
+            return res.status(403).json({
+                message: 'You are not the author of this post',
+                success: false
+            })
+        }
+
+        //delete post
+        await Post.findByIdAndDelete(postId);
+
+        //remove the post id from user's post
+        let user = await User.findById(authorId);
+        user.posts = user.posts.filter(id => id.toString() !== postId);
+        await user.save();
+
+        //delete associated comments
+        await Comment.deleteMany({ post: postId });
+
+
+        return res.status(200).json({
+            message: 'Post deleted successfully',
+            success: true
+        })
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const bookMarkPost = async (req, res) => {
+    try {
+        const postId = req.params.id;
+        const authorId = req.id;
+        const post = await Post.findById(postId);
+        if (!post) {
+            return res.status(404).json({
+                message: 'Post not found',
+                success: false
+            })
+        }
+
+        const user = await User.findById(authorId);
+        if (user.bookmarks.includes(post._id)) {
+            //already bookmark = remove from bok marks
+
+            await user.updateOne({ $pull: { bookmarks: post._id } });
+            await user.save();
+            return res.status(200).json({
+                message: 'Post removed from bookmarks',
+                success: true
+            })
+
+        } else {
+            // bookmarks added
+
+            await user.updateOne({ $addToSet: { bookmarks: post._id } });
+            await user.save();
+            return res.status(200).json({
+                message: 'Post saved in bookmarks',
+                success: true
+            })
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
